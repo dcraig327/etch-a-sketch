1c7180cf7f5aee74a304bd55cbf386044f2645ba
David Craig <37514553+dcraig327@users.noreply.github.com>
2022-11-05 14:02:41 -0700

    add reset and resize buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let gridSize = 16;
+
 // create 16x16 grid
 
 function removeBodyContent() {
@@ -27,23 +29,26 @@ function applyHover(e) {
   this.classList.add("hover");
 }
 
-function createGrid() {
-  let grid = createElement("div", "grid");
-  for (let i = 1; i <= 16; i++) {
-    let row = createElement("div", "grid-row");
-    row.classList.add(`grid-row-${i}`);
-    for (let j = 1; j <= 16; j++) {
-      let square = createElement("div", "square");
-      square.classList.add(`square-${i}-${j}`);
-      square.addEventListener("mouseover", applyHover, {
-        capture: true,
-        once: true,
-      });
-      row.appendChild(square);
-    }
-    grid.appendChild(row);
-  }
+function createGrid(gridSize = 16) {
+  const gridWidth = 800;
+  const squareWidth = gridWidth / gridSize;
 
+  let numSquares = gridSize * gridSize;
+  let grid = createElement("div", "grid");
+  grid.setAttribute("style", `width:${gridWidth}px; height: ${gridWidth}px`);
+  for (let i = 1; i <= numSquares; i++) {
+    let square = createElement("div", "square");
+    square.setAttribute(
+      "style",
+      `width:${squareWidth}px; height: ${squareWidth}px`
+    );
+    square.classList.add(`square-${i}`);
+    square.addEventListener("mouseover", applyHover, {
+      capture: true,
+      once: true,
+    });
+    grid.appendChild(square);
+  }
   document.body.appendChild(grid);
 }
 
@@ -52,8 +57,49 @@ function createHead() {
   addCssFile("style.css");
 }
 
+function clearGrid() {
+  let grid = document.querySelector(".grid");
+  grid.remove();
+}
+
+function buttonReset(e) {
+  //  let gridSize = prompt("What is the Grid Size?");
+  //  createGrid(gridSize);
+  clearGrid();
+  createGrid(gridSize);
+}
+function buttonResize(e) {
+  do {
+    gridSize = prompt("What is the Grid Size (16-100)?");
+  } while (gridSize < 16 || gridSize > 100);
+  buttonReset(e);
+}
+
+function createResetButton() {
+  let btn = createElement("button", "button-reset", "Reset");
+  btn.addEventListener("click", buttonReset);
+  let container = document.querySelector(".button-container");
+  container.appendChild(btn);
+}
+function createResizeButton() {
+  let btn = createElement("button", "button-resize", "Resize");
+  btn.addEventListener("click", buttonResize);
+  let container = document.querySelector(".button-container");
+  container.appendChild(btn);
+}
+function createButtonContainer() {
+  let container = createElement("div", "button-container");
+  document.body.appendChild(container);
+}
+function createButtons() {
+  createButtonContainer();
+  createResetButton();
+  createResizeButton();
+}
+
 function createBody() {
   removeBodyContent();
+  createButtons();
   createGrid();
 }
 

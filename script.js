205a3cd40f19c2fedd4b608380d582b1d306b356
David Craig <37514553+dcraig327@users.noreply.github.com>
2022-11-05 11:14:50 -0700

    add 16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,63 @@
+// create 16x16 grid
+
+function removeBodyContent() {
+  document.body.innerHTML = "";
+}
+
+function setTitle(title) {
+  document.title = title;
+}
+
+function addCssFile(fileName) {
+  let head = document.head;
+  let link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = fileName;
+  head.appendChild(link);
+}
+
+function createElement(elementName, className = "", text = "") {
+  const element = document.createElement(elementName);
+  if (className != "") element.classList.add(className);
+  if (text != "") element.textContent = text;
+  return element;
+}
+
+function createGrid() {
+  let grid = createElement("div", "grid");
+  for (let i = 1; i <= 16; i++) {
+    let row = createElement("div", "grid-row");
+    row.classList.add(`grid-row-${i}`);
+    for (let j = 1; j <= 16; j++) {
+      let square = createElement("div", "square");
+      square.classList.add(`square-${i}-${j}`);
+      row.appendChild(square);
+    }
+    grid.appendChild(row);
+  }
+
+  document.body.appendChild(grid);
+}
+
+function createHead() {
+  setTitle("Etch-A-Sketch");
+  addCssFile("style.css");
+}
+
+function createBody() {
+  removeBodyContent();
+  createGrid();
+}
+
+function createPage() {
+  createHead();
+  createBody();
+}
+
+/******************************************************************************/
+function main() {
+  createPage();
+}
+
+/******************************************************************************/
+main();
